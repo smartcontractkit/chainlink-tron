374bb35bd33644d72725b2d69cb9d338bb974b8f
cfal <cfal@users.noreply.github.com>
2024-05-15 15:11:18 +0400

    tron/scripts/java-tron.down.sh: cleanup all running java-tron nodes, prevent some errors

--- scripts/java-tron.down.sh
+++ scripts/java-tron.down.sh
@@ -1,18 +1,24 @@
 #!/usr/bin/env bash
-# TODO: this script needs to be replaced with a predefined K8s enviroment
 
 echo "Cleaning up java-tron container.."
 
-echo "Checking for existing 'chainlink-tron.java-tron' docker container..."
-dpid=`docker ps -a | grep chainlink-tron.java-tron | awk '{print $1}'`;
-if [ -z "$dpid" ]
-then
-    echo "No docker java-tron container running.";
+echo "Checking for existing 'chainlink-tron.java-tron' docker containers..."
+
+dpid=`docker ps -a | grep chainlink-tron.java-tron | awk '{print $1}'`
+
+if [ -z "$dpid" ]; then
+  echo "No docker java-tron containers running."
 else
-    docker kill $dpid;
-    docker rm $dpid;
+  for id in $dpid; do
+    echo "Killing docker container: ${id}"
+    docker kill $id || true
+    # Try to kill the docker container normally, if it doesn't work, use the --force flag to send a SIGKILL.
+    # docker rm --force always exits with code 0, even if the container no longer exists.
+    docker rm $id || docker rm --force $id
+  done
 fi
 
-docker network rm chainlink-tron.network
+echo "Removing network.."
+docker network rm --force chainlink-tron.network
 
 echo "Cleanup finished."
