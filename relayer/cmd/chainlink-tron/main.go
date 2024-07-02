c7c7e939ca56bf5750dd749f09ec04622e1a2d74
cfal <cfal@users.noreply.github.com>
2024-07-02 23:09:16 +0400

    tron/relayer/cmd/chainlink-tron/main.go: add plugin entrypoint

---
+++ relayer/cmd/chainlink-tron/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/go-plugin"
+	"github.com/pelletier/go-toml/v2"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/loop"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/core"
+
+	tronplugin "github.com/smartcontractkit/chainlink-internal-integrations/tron/relayer/plugin"
+)
+
+const (
+	loggerName = "PluginTron"
+)
+
+func main() {
+	s := loop.MustNewStartedServer(loggerName)
+	defer s.Stop()
+
+	p := &pluginRelayer{Plugin: loop.Plugin{Logger: s.Logger}}
+	defer s.Logger.ErrorIfFn(p.Close, "Failed to close")
+
+	s.MustRegister(p)
+
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	plugin.Serve(&plugin.ServeConfig{
+		HandshakeConfig: loop.PluginRelayerHandshakeConfig(),
+		Plugins: map[string]plugin.Plugin{
+			loop.PluginRelayerName: &loop.GRPCPluginRelayer{
+				PluginServer: p,
+				BrokerConfig: loop.BrokerConfig{
+					StopCh:   stopCh,
+					Logger:   s.Logger,
+					GRPCOpts: s.GRPCOpts,
+				},
+			},
+		},
+		GRPCServer: s.GRPCOpts.NewServer,
+	})
+}
+
+type pluginRelayer struct {
+	loop.Plugin
+}
+
+func (c *pluginRelayer) NewRelayer(ctx context.Context, config string, keystore loop.Keystore, capRegistry core.CapabilitiesRegistry) (loop.Relayer, error) {
+	d := toml.NewDecoder(strings.NewReader(config))
+	d.DisallowUnknownFields()
+	var cfg struct {
+		Tron tronplugin.TOMLConfig
+	}
+
+	if err := d.Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to decode config toml: %w:\n\t%s", err, config)
+	}
+
+	tronConfig := &cfg.Tron
+	if err := tronConfig.ValidateConfig(); err != nil {
+		return nil, fmt.Errorf("invalid tron config: %w", err)
+	}
+	if !tronConfig.IsEnabled() {
+		return nil, fmt.Errorf("cannot create new chain with ID %s: chain is disabled", *tronConfig.ChainID)
+	}
+
+	relayer := tronplugin.NewRelayer(tronConfig, c.Logger, keystore)
+	c.SubService(relayer)
+
+	return relayer, nil
+}
