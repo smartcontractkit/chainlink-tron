e13c6fa291e74d06ba5cb23d03050af225350ef4
yongkangchia <yongkangchia@smartcontract.com>
2024-08-06 16:24:00 +0800

    updated mock files

--- relayer/mocks/Reader.go
+++ relayer/mocks/Reader.go
@@ -14,34 +14,6 @@ type Reader struct {
 	mock.Mock
 }
 
-// Balance provides a mock function with given fields: _a0
-func (_m *Reader) Balance(_a0 address.Address) (int64, error) {
-	ret := _m.Called(_a0)
-
-	if len(ret) == 0 {
-		panic("no return value specified for Balance")
-	}
-
-	var r0 int64
-	var r1 error
-	if rf, ok := ret.Get(0).(func(address.Address) (int64, error)); ok {
-		return rf(_a0)
-	}
-	if rf, ok := ret.Get(0).(func(address.Address) int64); ok {
-		r0 = rf(_a0)
-	} else {
-		r0 = ret.Get(0).(int64)
-	}
-
-	if rf, ok := ret.Get(1).(func(address.Address) error); ok {
-		r1 = rf(_a0)
-	} else {
-		r1 = ret.Error(1)
-	}
-
-	return r0, r1
-}
-
 // BaseClient provides a mock function with given fields:
 func (_m *Reader) BaseClient() sdk.GrpcClient {
 	ret := _m.Called()
