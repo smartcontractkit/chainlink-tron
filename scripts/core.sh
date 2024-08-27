95c59fd0b2ff7b7736b798d9553f32554d7bbc47
Kristijan Rebernisak <kristijan.rebernisak@gmail.com>
2024-08-27 15:38:09 +0200

    nix flake update + script db check added

--- scripts/core.sh
+++ scripts/core.sh
@@ -47,8 +47,11 @@ for ((i = 1; i <= NODE_COUNT; i++)); do
 	database_name="tron_test_${i}"
 	echo "Creating database: ${database_name}"
 	host_postgres_url="postgresql://postgres:postgres@127.0.0.1:5432/postgres"
-	psql "${host_postgres_url}" -c "DROP DATABASE ${database_name};" &>/dev/null || true
-	psql "${host_postgres_url}" -c "CREATE DATABASE ${database_name};" &>/dev/null
+	psql "${host_postgres_url}" -c "DROP DATABASE ${database_name};"
+	# Create the database if it doesn't exist
+	psql "${host_postgres_url}" -tc "SELECT 1 FROM pg_database WHERE datname = '${database_name}'" \
+		| grep -q 1 \
+		|| psql "${host_postgres_url}" -c "CREATE DATABASE ${database_name};"
 
 	listen_args=()
 	for ip in $listen_ips; do
