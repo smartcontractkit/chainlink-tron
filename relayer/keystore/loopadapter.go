ff35c7f43fd7ca976d7e0fda1311e038f984f834
Kodey Kilday-Thomas <kodey.kildaythomas@smartcontract.com>
2025-04-17 11:54:04 +0100

    fix: pr comments

--- relayer/keystore/loopadapter.go
+++ relayer/keystore/loopadapter.go
@@ -2,27 +2,31 @@ package keystore
 
 import (
 	"context"
+	"fmt"
 
 	tronsdk "github.com/fbsobreira/gotron-sdk/pkg/address"
+	"github.com/smartcontractkit/chainlink-common/pkg/types/core"
 	"github.com/smartcontractkit/chainlink-evm/pkg/keys"
 )
 
+var _ core.Keystore = (*loopKeystoreAdapter)(nil)
+
 // LoopKeystoreAdapter is an adapter that allows the EVM keystore to be used by the Tron TXM
 // It handles the conversion between tron addresses and evm addresses whilst delegating the signing to the EVM keystore
-type LoopKeystoreAdapter struct {
+type loopKeystoreAdapter struct {
 	ks keys.Store
 }
 
 // Creates a new LoopKeystoreAdapter which allows the EVM keystore to be used by the Tron TXM
-func NewLoopKeystoreAdapter(ks keys.Store) *LoopKeystoreAdapter {
-	return &LoopKeystoreAdapter{ks: ks}
+func NewLoopKeystoreAdapter(ks keys.Store) core.Keystore {
+	return &loopKeystoreAdapter{ks: ks}
 }
 
 // Accounts returns the list of enabled addresses from the keystore
-func (l *LoopKeystoreAdapter) Accounts(ctx context.Context) (accounts []string, err error) {
+func (l *loopKeystoreAdapter) Accounts(ctx context.Context) (accounts []string, err error) {
 	enabledAddresses, err := l.ks.EnabledAddresses(ctx)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to get enabled addresses: %w", err)
 	}
 
 	for _, address := range enabledAddresses {
@@ -31,11 +35,11 @@ func (l *LoopKeystoreAdapter) Accounts(ctx context.Context) (accounts []string, 
 	return accounts, nil
 }
 
-func (l *LoopKeystoreAdapter) Sign(ctx context.Context, account string, data []byte) (signed []byte, err error) {
+func (l *loopKeystoreAdapter) Sign(ctx context.Context, account string, data []byte) (signed []byte, err error) {
 	// We'll need to convert the tron address to an evm address to sign the data
 	tronAddr, err := tronsdk.Base58ToAddress(account)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to convert to TRON address: %w", err)
 	}
 
 	addr := tronAddr.EthAddress()
